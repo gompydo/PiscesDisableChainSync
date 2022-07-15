47b66612c528d3ac111ef76265f4eecf5673c26e
gompydo <100021042+gompydo@users.noreply.github.com>
2022-07-15 02:19:00 +0200

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading the new sys.config"
-curl -sLf https://raw.githubusercontent.com/gompydo/PiscesDisableChainSync/sys.config -o /home/pi/hnt/miner/configs/sys.config
+curl -sLf https://raw.githubusercontent.com/gompydo/PiscesDisableChainSync/main/sys.config -o /home/pi/hnt/miner/configs/sys.config
 echo -n "Restarting miner..."
 docker restart miner
 echo "Script Complete!"
