2bb57424a62987370f71b88d1bb9d15022518b97
gompydo <100021042+gompydo@users.noreply.github.com>
2022-07-15 23:54:35 +0200

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 echo "Downloading the new sys.config"
 curl -sLf https://raw.githubusercontent.com/gompydo/PiscesDisableChainSync/main/sys.config -o /home/pi/hnt/miner/configs/sys.config
 echo -n "Restarting miner..."
