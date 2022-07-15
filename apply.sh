52c7de38b952fbdaa623dde6061772100cfbc6a6
gompydo <100021042+gompydo@users.noreply.github.com>
2022-07-16 00:33:12 +0200

    Delete apply.sh

--- apply.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Downloading the new sys.config"
-curl -sLf https://raw.githubusercontent.com/gompydo/PiscesDisableChainSync/main/sys.config -o /home/pi/hnt/miner/configs/sys.config
-echo -n "Restarting miner..."
-docker restart miner
-echo "Script Complete!"
